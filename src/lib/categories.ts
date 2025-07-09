353bf4c66bbc7dfdec03880cccbee8491796b7b9
Md Kasif Uddin <mdkasifuddin121@gmail.com>
2025-07-09 10:19:59 +0000

    Scheduled Fetching Logic
    Fetch news every 2 hours (12 times/day)
    
    At ea

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,13 +1,12 @@
-
-import { Globe, Landmark, Rocket, Palette, Atom, Lightbulb, HeartPulse, Scale } from "lucide-react";
+import { Globe, Landmark, Rocket, Palette, Atom, HeartPulse, Scale } from "lucide-react";
 
 export const newsCategories = [
     { name: "For You", slug: "top", icon: Globe },
+    { name: "World", slug: "world", icon: Globe },
     { name: "Business", slug: "business", icon: Landmark },
     { name: "Technology", slug: "technology", icon: Rocket },
     { name: "Entertainment", slug: "entertainment", icon: Palette },
     { name: "Sports", slug: "sports", icon: Atom },
-    { name: "Science", slug: "science", icon: Lightbulb },
     { name: "Health", slug: "health", icon: HeartPulse },
     { name: "Politics", slug: "politics", icon: Scale },
 ];
