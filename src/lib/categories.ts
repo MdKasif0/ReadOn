927f06804ab1faa4494eacc72d17981b8b613111
Md Kasif Uddin <mdkasifuddin121@gmail.com>
2025-07-10 12:36:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -5,14 +5,13 @@ export const newsCategories = [
     { name: "General", slug: "general", icon: Globe },
     { name: "World", slug: "world", icon: Globe },
     { name: "Nation", slug: "nation", icon: Landmark },
-    { name: "Business", slug: "business", icon: Landmark },
+    { name: "Business", slug: "business", icon: Briefcase },
     { name: "Technology", slug: "technology", icon: Rocket },
     { name: "Entertainment", slug: "entertainment", icon: Palette },
     { name: "Sports", slug: "sports", icon: Atom },
     { name: "Science", slug: "science", icon: Atom },
     { name: "Health", slug: "health", icon: HeartPulse },
     { name: "Politics", slug: "politics", icon: Scale },
-    { name: "Finance", slug: "business", icon: Briefcase }, // Re-using business for finance as API may not have finance
-    { name: "Fashion", slug: "entertainment", icon: Shirt }, // Re-using entertainment
-    { name: "Education", slug: "science", icon: GraduationCap } // Re-using science
+    { name: "Fashion", slug: "fashion", icon: Shirt }, 
+    { name: "Education", slug: "education", icon: GraduationCap }
 ];
